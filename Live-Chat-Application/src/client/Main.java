e5a2cf5998b5a4fbe6aaa6d70550354e5081a750
jithmiw <wjithmiravihari@gmail.com>
2022-08-08 17:04:49 +0530

    Updated the content of Main

--- Live-Chat-Application/src/client/Main.java
+++ Live-Chat-Application/src/client/Main.java
@@ -1,4 +1,23 @@
 package client;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/client/views/LoginForm.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Live-Chat");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
 }
