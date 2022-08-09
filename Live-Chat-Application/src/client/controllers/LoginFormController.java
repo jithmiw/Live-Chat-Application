30642fcac77bf09602eae915cd356b4a7e4e901b
jithmiw <wjithmiravihari@gmail.com>
2022-08-09 16:07:00 +0530

    Updated the content of LoginFormController with two methodsprivate void textFieldLimit(TextField textField, int limit){

--- Live-Chat-Application/src/client/controllers/LoginFormController.java
+++ Live-Chat-Application/src/client/controllers/LoginFormController.java
@@ -1,9 +1,13 @@
 package client.controllers;
 
+import client.Main;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
@@ -15,6 +19,32 @@ public class LoginFormController {
     public VBox vbox;
     public AnchorPane anchor;
     public ImageView img;
+
+    public void initialize()  {
+        img.setImage(new Image(Main.class.getResource("resources/img/messages.png").toString()));
+
+        textFieldLimit(fieldUserName, 10);
+        textFieldLimit(fieldIpAddress, 20);
+        textFieldLimit(fieldPort, 10);
+
+        Platform.runLater(() -> fieldUserName.requestFocus());
+
+        vbox.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                buttonEnter.fire();
+            }
+        });
+    }
+
+    private void textFieldLimit(TextField textField, int limit){
+        textField.lengthProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue.intValue() > oldValue.intValue()){
+                if(textField.getText().length() >= 10){
+                    textField.setText(textField.getText().substring(0,limit));
+                }
+            }
+        });
+    }
 
     public void pressedEnter(ActionEvent event) {
     }
