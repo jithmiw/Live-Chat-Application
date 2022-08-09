296f50875c2d6531937769d04ff481f50db30172
jithmiw <wjithmiravihari@gmail.com>
2022-08-09 16:04:08 +0530

    Created fields and methods in LoginFormController

--- Live-Chat-Application/src/client/controllers/LoginFormController.java
+++ Live-Chat-Application/src/client/controllers/LoginFormController.java
@@ -1,4 +1,21 @@
 package client.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
 public class LoginFormController {
+    public TextField fieldIpAddress;
+    public Button buttonEnter;
+    public TextField fieldUserName;
+    public TextField fieldPort;
+    public VBox vbox;
+    public AnchorPane anchor;
+    public ImageView img;
+
+    public void pressedEnter(ActionEvent event) {
+    }
 }
