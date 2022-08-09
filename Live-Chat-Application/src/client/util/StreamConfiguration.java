1360eea016137eae101a2116d115377d1cb58dbb
jithmiw <wjithmiravihari@gmail.com>
2022-08-09 16:18:27 +0530

    Created fields of StreamConfiguration

--- Live-Chat-Application/src/client/util/StreamConfiguration.java
+++ Live-Chat-Application/src/client/util/StreamConfiguration.java
@@ -1,4 +1,14 @@
 package client.util;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class StreamConfiguration {
+    private Socket socket;
+    private String hostAddr;
+    private int portAddr = 0;
+    private String name;
+    private DataInputStream streamIn;
+    private DataOutputStream streamOut;
 }
