ae0b80150e7c38279511ae6a156b0c32f7d3e335
jithmiw <wjithmiravihari@gmail.com>
2022-08-09 16:21:21 +0530

    Updated the content of StreamConfiguration with two methods

--- Live-Chat-Application/src/client/util/StreamConfiguration.java
+++ Live-Chat-Application/src/client/util/StreamConfiguration.java
@@ -1,14 +1,37 @@
 package client.util;
 
+import javafx.scene.control.Alert;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class StreamConfiguration {
     private Socket socket;
     private String hostAddr;
-    private int portAddr = 0;
+    private final int portAddr = 0;
     private String name;
     private DataInputStream streamIn;
     private DataOutputStream streamOut;
+
+    public void initSocket() throws IOException {
+        if (hostAddr.isEmpty() || portAddr == 0) {
+            new Alert(Alert.AlertType.INFORMATION, "Invalid host address or port address. Please try again...").show();
+            return;
+        }
+        this.socket = new Socket(hostAddr, portAddr);
+    }
+
+    public void initStream() {
+        try {
+            streamIn = new DataInputStream(socket.getInputStream());
+            streamOut = new DataOutputStream(socket.getOutputStream());
+            streamOut.writeUTF(name);
+            streamOut.flush();
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR, "An error occurred while initializing streams. Please check the server and try again...").show();
+            System.exit(0);
+        }
+    }
 }
