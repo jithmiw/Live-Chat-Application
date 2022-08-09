398c4cf422eb37581113312cb0af9a9830efe5ab
jithmiw <wjithmiravihari@gmail.com>
2022-08-09 16:25:33 +0530

    Updated ApplicationContext

--- Live-Chat-Application/src/client/ApplicationContext.java
+++ Live-Chat-Application/src/client/ApplicationContext.java
@@ -1,4 +1,11 @@
 package client;
 
+import client.util.StreamConfiguration;
+
 public class ApplicationContext {
+    private static final StreamConfiguration streamConfiguration = new StreamConfiguration();
+
+    public static StreamConfiguration getStreamConfiguration() {
+        return streamConfiguration;
+    }
 }
