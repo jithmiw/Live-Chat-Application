8ed929eb8804a2f38381130f150513b3bd76c23f
jithmiw <wjithmiravihari@gmail.com>
2022-08-09 16:32:20 +0530

    Updated LoginFormController

--- Live-Chat-Application/src/client/controllers/LoginFormController.java
+++ Live-Chat-Application/src/client/controllers/LoginFormController.java
@@ -1,8 +1,14 @@
 package client.controllers;
 
+import client.ApplicationContext;
 import client.Main;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -10,6 +16,9 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public TextField fieldIpAddress;
@@ -20,7 +29,7 @@ public class LoginFormController {
     public AnchorPane anchor;
     public ImageView img;
 
-    public void initialize()  {
+    public void initialize() {
         img.setImage(new Image(Main.class.getResource("resources/img/messages.png").toString()));
 
         textFieldLimit(fieldUserName, 10);
@@ -36,16 +45,52 @@ public class LoginFormController {
         });
     }
 
-    private void textFieldLimit(TextField textField, int limit){
+    private void textFieldLimit(TextField textField, int limit) {
         textField.lengthProperty().addListener((observable, oldValue, newValue) -> {
-            if(newValue.intValue() > oldValue.intValue()){
-                if(textField.getText().length() >= 10){
-                    textField.setText(textField.getText().substring(0,limit));
+            if (newValue.intValue() > oldValue.intValue()) {
+                if (textField.getText().length() >= 10) {
+                    textField.setText(textField.getText().substring(0, limit));
                 }
             }
         });
     }
 
     public void pressedEnter(ActionEvent event) {
+        String name;
+        int port;
+        String ip;
+        try {
+            name = fieldUserName.getText();
+            port = Integer.parseInt(fieldPort.getText());
+            ip = fieldIpAddress.getText();
+        } catch (Exception e) {
+            new Alert(Alert.AlertType.INFORMATION, "Invalid input. Please try again...").show();
+            e.printStackTrace();
+            return;
+        }
+        try {
+            initStreamConfiguration(name, ip, port);
+            Parent window = FXMLLoader.load(getClass().getResource("/client/views/ClientForm2.fxml"));
+            Scene newScene = new Scene(window);
+            Stage mainWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            mainWindow.setTitle("Live-Chat");
+            mainWindow.setScene(newScene);
+            mainWindow.setMaximized(true);
+        } catch (IOException ex) {
+            new Alert(Alert.AlertType.ERROR, "An error occurred.").show();
+            ex.printStackTrace();
+        } catch (Exception e) {
+            new Alert(Alert.AlertType.ERROR, "An error occurred.").show();
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    private void initStreamConfiguration(String name, String ip, int port) throws IOException {
+        ApplicationContext.getStreamConfiguration().setName(name);
+        ApplicationContext.getStreamConfiguration().setHostAddr(ip);
+        ApplicationContext.getStreamConfiguration().setPortAddr(port);
+        ApplicationContext.getStreamConfiguration().initSocket();
+        ApplicationContext.getStreamConfiguration().initStream();
     }
 }
