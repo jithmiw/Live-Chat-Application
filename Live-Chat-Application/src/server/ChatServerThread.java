7bde71b42cff2c3199285d17abaa167204e890be
jithmiw <wjithmiravihari@gmail.com>
2022-08-10 16:59:59 +0530

    Created fields in ChatServerThread

--- Live-Chat-Application/src/server/ChatServerThread.java
+++ Live-Chat-Application/src/server/ChatServerThread.java
@@ -1,4 +1,15 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class ChatServerThread {
+    private final ChatServer server = null;
+    private final Socket socket = null;
+    private final int ID = -1;
+    private final DataInputStream streamIn = null;
+    private final DataOutputStream streamOut = null;
+    private final String clientName = null;
+    private final boolean haveName = false;
 }
