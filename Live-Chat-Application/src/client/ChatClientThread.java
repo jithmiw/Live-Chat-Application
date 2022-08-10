bce68c0c0bec6f14318d757965d17cb673450ee9
jithmiw <wjithmiravihari@gmail.com>
2022-08-10 17:15:47 +0530

    Updated the content of ChatClientThread

--- Live-Chat-Application/src/client/ChatClientThread.java
+++ Live-Chat-Application/src/client/ChatClientThread.java
@@ -1,4 +1,16 @@
 package client;
 
+import client.controllers.ClientFormController;
+
+import java.io.DataInputStream;
+
 public class ChatClientThread {
+
+    private final ClientFormController client;
+    private final DataInputStream streamIn;
+
+    public ChatClientThread(ClientFormController client) {
+        this.client = client;
+        streamIn = ApplicationContext.getStreamConfiguration().getStreamIn();
+    }
 }
