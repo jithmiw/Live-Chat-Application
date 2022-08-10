6c100f1a265f510f4ef8f1fb4221d3640d739e6c
jithmiw <wjithmiravihari@gmail.com>
2022-08-10 17:03:40 +0530

    Updated the content of ChatServerThread

--- Live-Chat-Application/src/server/ChatServerThread.java
+++ Live-Chat-Application/src/server/ChatServerThread.java
@@ -5,11 +5,74 @@ import java.io.DataOutputStream;
 import java.net.Socket;
 
 public class ChatServerThread {
-    private final ChatServer server = null;
-    private final Socket socket = null;
-    private final int ID = -1;
-    private final DataInputStream streamIn = null;
-    private final DataOutputStream streamOut = null;
-    private final String clientName = null;
-    private final boolean haveName = false;
+    private ChatServer server = null;
+    private Socket socket = null;
+    private int ID = -1;
+    private DataInputStream streamIn = null;
+    private DataOutputStream streamOut = null;
+    private String clientName = null;
+    private boolean haveName = false;
+
+    public ChatServerThread(ChatServer server, Socket socket) {
+        super();
+        this.server = server;
+        this.socket = socket;
+        ID = socket.getPort();
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public ChatServer getServer() {
+        return server;
+    }
+
+    public void setServer(ChatServer server) {
+        this.server = server;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public DataInputStream getStreamIn() {
+        return streamIn;
+    }
+
+    public void setStreamIn(DataInputStream streamIn) {
+        this.streamIn = streamIn;
+    }
+
+    public DataOutputStream getStreamOut() {
+        return streamOut;
+    }
+
+    public void setStreamOut(DataOutputStream streamOut) {
+        this.streamOut = streamOut;
+    }
+
+    public boolean isHaveName() {
+        return haveName;
+    }
+
+    public void setHaveName(boolean haveName) {
+        this.haveName = haveName;
+    }
 }
