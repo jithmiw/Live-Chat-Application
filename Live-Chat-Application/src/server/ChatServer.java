3842222c3eee5711cfdc6687821b424856edc900
jithmiw <wjithmiravihari@gmail.com>
2022-08-10 17:06:14 +0530

    Updated the content of ChatServer

--- Live-Chat-Application/src/server/ChatServer.java
+++ Live-Chat-Application/src/server/ChatServer.java
@@ -1,4 +1,25 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class ChatServer {
+    private ChatServerThread[] clients = new ChatServerThread[50];
+    private ServerSocket server = null;
+    private Thread thread = null;
+    private int clientCount = 0;
+
+    public static void main(String[] args) {
+        new ChatServer(5000);
+    }
+
+    public ChatServer(int port) {
+        try {
+            System.out.println("Binding to port " + port + ", please wait...");
+            server = new ServerSocket(port);
+            System.out.println("Server started: " + server);
+        } catch (IOException e) {
+            System.out.println("Cannot bind to port " + port + ": " + e.getMessage());
+        }
+    }
 }
