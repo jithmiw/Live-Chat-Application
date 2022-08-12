ff4b4fbc8d6c51082664d18d81dcedc9b27ef7e0
jithmiw <wjithmiravihari@gmail.com>
2022-08-12 18:21:59 +0530

    Updated the content of MessageConfiguration

--- Live-Chat-Application/src/client/util/MessageConfiguration.java
+++ Live-Chat-Application/src/client/util/MessageConfiguration.java
@@ -1,5 +1,6 @@
 package client.util;
 
+import com.jfoenix.effects.JFXDepthManager;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -9,6 +10,23 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
 public class MessageConfiguration {
+    private void createLabel(ListView<HBox> listView, String text, Pos pos) {
+        HBox hBox = new HBox();
+        Label l1 = new Label();
+
+        l1.setText(text);
+        l1.setFont(Font.font("System", FontWeight.NORMAL, 17));
+        l1.setStyle("-fx-background-color: #005C4B; -fx-background-radius: 13px; -fx-padding: 10px");
+        l1.setTextFill(Color.web("#ffffff"));
+        l1.setWrapText(true);
+        hBox.setMaxWidth(listView.getWidth() - 20);
+        hBox.setAlignment(pos);
+        hBox.getChildren().add(l1);
+        JFXDepthManager.setDepth(hBox, 2);
+        listView.getItems().add(hBox);
+        listView.scrollTo(listView.getItems().lastIndexOf(hBox));
+    }
+
     public void getClientNotification(ListView<HBox> listView, String text) {
         HBox hBox = new HBox();
         Label l1 = new Label();
@@ -21,5 +39,9 @@ public class MessageConfiguration {
         hBox.getChildren().add(l1);
         listView.getItems().add(hBox);
         listView.scrollTo(listView.getItems().lastIndexOf(hBox));
+    }
+
+    public void getServerRespond(ListView<HBox> listView, String text) {
+        createLabel(listView, " " + text, Pos.TOP_LEFT);
     }
 }
