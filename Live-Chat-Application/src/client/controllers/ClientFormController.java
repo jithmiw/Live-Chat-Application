79172fb16784ed74c4abd37cd96ed9cdd0273690
jithmiw <wjithmiravihari@gmail.com>
2022-08-16 12:21:36 +0530

    Updated the content of ClientFormController

--- Live-Chat-Application/src/client/controllers/ClientFormController.java
+++ Live-Chat-Application/src/client/controllers/ClientFormController.java
@@ -18,11 +18,15 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 
 public class ClientFormController {
+    private final FileChooser fileChooser = new FileChooser();
     public JFXButton buttonSend;
     public ListView<HBox> listView;
     public JFXTextField textField;
@@ -33,7 +37,10 @@ public class ClientFormController {
     public Label nameLabel;
     public ImageView logo;
     public ImageView imageSelect;
+    public Label lbl1, lbl2, lbl3, lbl4, lbl5, lbl6, lbl7, lbl8, lbl9, lbl10, lbl11, lbl12, lbl13, lbl14, lbl15,
+            lbl16, lbl17, lbl18, lbl19, lbl20, lbl21, lbl22, lbl23, lbl24, lbl25, lbl26, lbl27, lbl28;
 
+    Stage stage = new Stage();
     private String name;
     private DataOutputStream streamOut;
 
@@ -45,20 +52,13 @@ public class ClientFormController {
         nameLabel.setText(name);
 
         initFields();
+        initEmojis();
         new ChatClientThread(this);
         ClientNotification("Establishing connection. Please wait...");
         ClientNotification("Welcome " + name);
     }
 
     private void initFields() {
-        //Get focus except on control, shift, alt and capslock keys.
-        listView.setOnKeyPressed(event -> {
-            if (event.getCode() != KeyCode.CONTROL && event.getCode() != KeyCode.SHIFT && event.getCode() != KeyCode.ALT
-                    && event.getCode() != KeyCode.CAPS)
-                textField.requestFocus();
-
-        });
-
         //Textfield limit is 240 characters.
         textField.lengthProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue.intValue() > oldValue.intValue()) {
@@ -76,6 +76,20 @@ public class ClientFormController {
             }
         });
         Platform.runLater(() -> textField.requestFocus());
+    }
+
+    private void initEmojis() {
+        Label[] labels = new Label[]{lbl1, lbl2, lbl3, lbl4, lbl5, lbl6, lbl7, lbl8, lbl9, lbl10,
+                lbl11, lbl12, lbl13, lbl14, lbl15, lbl16, lbl17, lbl18,
+                lbl19, lbl20, lbl21, lbl22, lbl23, lbl24, lbl25, lbl26, lbl27, lbl28};
+        String[] unicode = new String[]{"\uD83D\uDE00", "\uD83D\uDE04", "\uD83D\uDE01", "\uD83D\uDE06", "\uD83D\uDE05", "\uD83D\uDE02",
+                "\uD83E\uDD23", "\uD83D\uDE07", "\uD83D\uDE0D", "\uD83D\uDE11", "\uD83D\uDE36", "\uD83D\uDE0F", "\uD83D\uDE2E", "\uD83D\uDE44",
+                "\uD83D\uDE34", "\uD83D\uDE2C", "\uD83D\uDE2D", "\uD83D\uDE31", "\uD83D\uDC40", "\uD83D\uDC4B", "\uD83D\uDC4D",
+                "\uD83D\uDC4E", "\uD83D\uDC4F", "\uD83D\uDE4F", "\uD83D\uDCAA", "\uD83E\uDD19", "\uD83E\uDD1D", "\uD83D\uDC4A"};
+
+        for (int i = 0; i < 28; i++) {
+            labels[i].setText(unicode[i]);
+        }
     }
 
     private void ClientNotification(String text) {
@@ -120,6 +134,15 @@ public class ClientFormController {
         ApplicationContext.getMessageConfiguration().getClientRespond(listView, text);
     }
 
+    public void emojiOnAction(MouseEvent event) {
+        Label label = (Label) event.getSource();
+        textField.setText(textField.getText() + label.getText());
+        textField.end();
+    }
+
     public void imgChooseOnAction(MouseEvent event) {
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+        }
     }
 }
